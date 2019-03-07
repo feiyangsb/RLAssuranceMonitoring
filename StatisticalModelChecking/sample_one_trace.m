bdf6505d5efeba8168f107b69d9dac6aebf8ba27
Feiyang Cai <feiyang.cai@vanderbilt.edu>
2019-03-07 16:29:49 -0600

    sample one trace

---
+++ StatisticalModelChecking/sample_one_trace.m
@@ -0,0 +1,24 @@
+clear t
+clear result
+
+sampleCommand = "Sample";
+t = tcpip('localhost', 3389, 'NetworkRole', 'client', 'InputBufferSize', 3000);
+fopen(t)
+
+fwrite(t, sampleCommand);
+while(1)
+    if (t.BytesAvailable ~= 0)
+        result = fread(t, t.BytesAvailable/4, 'single');
+        distance = result(1:length(result)/4);
+        velocity = result(length(result)/4+1:length(result)/4 * 2);
+        acceleration = result(length(result)/4*2+1:length(result)/4 * 3);
+        ttc_r = result(length(result)/4*3+1:length(result)/4 * 4);
+        break;
+    end
+end
+
+for i=1:length(distance)
+    %TTC(i) = calculateTTC(Dist(i), Vel(i), Acc(i));
+    TTC_r(i) = calculateTTC_r(distance(i), velocity(i), acceleration(i));
+end
+fclose(t)

d27f6c6e88ccd1a96f12adc575df325d0ed0dfa5
Feiyang Cai <feiyang.cai@vanderbilt.edu>
2019-03-07 16:29:08 -0600

    sample the trace from simulator and estimate the probability interval

--- StatisticalModelChecking/plugin.m
+++ StatisticalModelChecking/plugin.m
@@ -1,13 +1,15 @@
 
 delta = 0.01; %half interval size
 c = 0.99; %interval coefficient
-alpha = 100;
+alpha = 1;
 beta = 1;
 n = 0;
 x = 0;
+p_list = [];
+gamma_list = [];
 
 sampleCommand = "Sample";
-t = tcpip('localhost', 3389, 'NetworkRole', 'client');
+t = tcpip('localhost', 3389, 'NetworkRole', 'client', 'InputBufferSize', 3000);
 fopen(t)
 
 while(1)
@@ -17,12 +19,15 @@ while(1)
     %waiting for result
     while(1)
         if (t.BytesAvailable ~= 0)
-            result = fread(t, t.BytesAvailable);
-            result = convertCharsToStrings(char(result));
+            result = fread(t, t.BytesAvailable/4, 'single');
+            distance = result(1:length(result)/4);
+            velocity = result(length(result)/4+1:length(result)/4 * 2);
+            acceleration = result(length(result)/4*2+1:length(result)/4 * 3);
+            ttc_r = result(length(result)/4*3+1:length(result)/4 * 4);
             break;
         end
     end
-    if (result == "Yes") %satisfying the specification
+    if (all(ttc_r < 2.0)) %satisfying the specification
         x=x+1;
     end
     n=n+1;
@@ -30,9 +35,12 @@ while(1)
     p = (x+alpha)/(n+alpha+beta);
     t0 = max(0, p-delta);
     t1 = min(1, p+delta);
-    
+    p_list(end+1) = p;
     pd = makedist('Beta','a',alpha+x,'b',beta+n-x);
-    gamma = cdf(pd,t1) - cdf(pd,t0)
+    gamma = cdf(pd,t1) - cdf(pd,t0);
+    gamma_list(end+1) = gamma;
+    INFO = ['# of Episode: ', num2str(n), '; # of Satisfying: ', num2str(x), '; Probability: ', num2str(p), '; Confidence(%): ', num2str(gamma)];
+    disp(INFO)
     if (gamma >= c)
         break;
     end

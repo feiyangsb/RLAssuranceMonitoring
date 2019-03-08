c13d1925ebba8f0e3501cac9b2eaeab0ad622dda
Feiyang Cai <feiyang.cai@vanderbilt.edu>
2019-03-07 22:49:48 -0600

    revise one sample code

--- StatisticalModelChecking/sample_one_trace.m
+++ StatisticalModelChecking/sample_one_trace.m
@@ -8,6 +8,10 @@ fopen(t)
 fwrite(t, sampleCommand);
 while(1)
     if (t.BytesAvailable ~= 0)
+        number_data = fread(t,1,'single');
+        while (t.BytesAvailable ~= number_data*4)
+            t.BytesAvailable;
+        end
         result = fread(t, t.BytesAvailable/4, 'single');
         distance = result(1:length(result)/4);
         velocity = result(length(result)/4+1:length(result)/4 * 2);
